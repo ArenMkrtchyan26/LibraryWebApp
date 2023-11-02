a50a1a578b08b38baa60800739cf5f0e38b637e4
Haykuhi Zohrabyan <haykuhiz@mail.ru>
2023-11-02 17:54:03 +0400

    cheng

--- LibraryDAL/Entities/Book.cs
+++ LibraryDAL/Entities/Book.cs
@@ -10,7 +10,6 @@ namespace LibraryDAL.Entities
     public class Book
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
         public string AuthorName { get; set; }
         public decimal Price { get; set; }
